934c12201362e9663a71c5f817f29dd9f33a9111
noah415 <noah.otsuka@gmail.com>
2021-05-25 17:34:25 -0700

    initial commit

--- lab5.java
+++ lab5.java
@@ -1,8 +1,34 @@
 
 class lab5
 {
+
+    private static int getTag(String line, int KB, int wordBlocks)
+    {
+        
+    }
+
+    private static void processLine(String line)
+    {
+        int tag;
+
+        // calculate and mod tag for cache #1
+        tag = getTag(line, 2, 1);
+
+        // calculate and mod tag for cache #2
+        // calculate and mod tag for cache #3
+        // calculate and mod tag for cache #4
+        // calculate and mod tag for cache #5
+        // calculate and mod tag for cache #6
+        // calculate and mod tag for cache #7
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("hello world");
+        Scanner scanner = new Scanner(args[1]);
+
+        while (scanner.hasNextLine())
+        {
+            processLine(scanner.nextLine());
+        }
     }
 }
